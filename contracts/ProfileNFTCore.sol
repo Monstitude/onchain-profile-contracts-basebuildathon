7beab1a0fae84baaf91efc6b8c6995d5dca704d7
Monstitude <monstitude@gmail.com>
2025-04-27 11:31:20 +0800

    feat: mint

--- contracts/ProfileNFTCore.sol
+++ contracts/ProfileNFTCore.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.22;
 
 import { ONFT721 } from "@layerzerolabs/onft-evm/contracts/onft721/ONFT721.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ProfileNFTCore is ONFT721 {
+    uint256 private _tokenIds;
+
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
     ) ONFT721(_name, _symbol, _lzEndpoint, _delegate) {}
+
+    function mint(address _to) public {
+        _tokenIds++;
+        uint256 newProfileId = _tokenIds;
+
+        _mint(_to, newProfileId);
+    }
 }
